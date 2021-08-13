2b7e5eb4684b5b2f68858ec394f952add7f5edd5
kayraberktuncer <kayrabtuncer@gmail.com>
2021-08-13 20:29:11 +0300

    Add navbar style

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -41,14 +41,16 @@ export default function GamePage({ seconds, minutes, hours, reset }) {
   return (
     <div className="game-page">
       <NavBar seconds={seconds} minutes={minutes} hours={hours} reset={reset} />
-      {cards.hasOwnProperty("decks") && game.decks[10].length > 0 && (
-        <div
-          onClick={e => {
-            distributeRemCards(game, setGame);
-          }}
-          className="card card__down card__remcards"
-        ></div>
-      )}
+      <div className="game-tools">
+        {cards.hasOwnProperty("decks") && game.decks[10].length > 0 && (
+          <div
+            onClick={e => {
+              distributeRemCards(game, setGame);
+            }}
+            className="card card__down card__remcards"
+          ></div>
+        )}
+      </div>
       <div className="game-content">
         {cards.hasOwnProperty("decks") &&
           game.decks.slice(0, 10).map((deck, index) => (
