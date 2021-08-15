d15b6c9ff9ed5263293a9d9cda51d202f4eb411e
kayraberktuncer <kayrabtuncer@gmail.com>
2021-08-15 17:43:19 +0300

    Fix csstransition error

--- src/components/GameContent.js
+++ src/components/GameContent.js
@@ -1,7 +1,6 @@
 import { dragStart, drag, dragEnter, selectCard, drop } from "../logic/index";
 import Card from "../components/Card";
 import CardHolder from "../components/CardHolder";
-import CSSTransitionGroup from "react-addons-css-transition-group";
 
 export function GameContent({ cards, game, setGame }) {
   return (
@@ -24,47 +23,39 @@ export function GameContent({ cards, game, setGame }) {
               </div>
             ) : (
               <div key={index + " 2"} deck={deck}>
-                <CSSTransitionGroup
-                  transitionName="card"
-                  transitionEnterTimeout={500}
-                  transitionLeaveTimeout={300}
-                  key={index + " 3"}
-                  deck={deck}
-                >
-                  {deck.map(card => (
-                    <div
-                      key={card.rank + " " + card.suit + " " + card.deck + " 0"}
-                      id={card.rank + " " + card.suit + " " + card.deck}
-                      className="card__wrapper card__stack"
-                      draggable={true}
-                      onDragStart={e => {
-                        dragStart(e, card, deck, game, setGame);
-                      }}
-                      onDrag={e => {
-                        drag(e, card, game, setGame);
-                      }}
-                      onDragEnter={e => {
-                        if (card.isDown === false) {
-                          dragEnter(e, game, setGame, card, deck);
-                        }
-                      }}
-                      onDragEnd={e => {
-                        drop(e, card, game, setGame);
-                      }}
-                      onClick={e => {
-                        selectCard(card, deck, null, game, setGame);
-                      }}
-                    >
-                      <Card
-                        key={card.rank + " " + card.suit + " " + card.deck}
-                        card={card}
-                        isSelected={card.isSelected}
-                        isDown={card.isDown}
-                        isHighlighted={card.isHighlighted}
-                      />
-                    </div>
-                  ))}
-                </CSSTransitionGroup>
+                {deck.map(card => (
+                  <div
+                    key={card.rank + " " + card.suit + " " + card.deck + " 0"}
+                    id={card.rank + " " + card.suit + " " + card.deck}
+                    className="card__wrapper card__stack"
+                    draggable={true}
+                    onDragStart={e => {
+                      dragStart(e, card, deck, game, setGame);
+                    }}
+                    onDrag={e => {
+                      drag(e, card, game, setGame);
+                    }}
+                    onDragEnter={e => {
+                      if (card.isDown === false) {
+                        dragEnter(e, game, setGame, card, deck);
+                      }
+                    }}
+                    onDragEnd={e => {
+                      drop(e, card, game, setGame);
+                    }}
+                    onClick={e => {
+                      selectCard(card, deck, null, game, setGame);
+                    }}
+                  >
+                    <Card
+                      key={card.rank + " " + card.suit + " " + card.deck}
+                      card={card}
+                      isSelected={card.isSelected}
+                      isDown={card.isDown}
+                      isHighlighted={card.isHighlighted}
+                    />
+                  </div>
+                ))}
               </div>
             )}
           </>
